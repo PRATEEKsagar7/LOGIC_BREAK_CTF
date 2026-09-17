189eaf20e5bf1ec8a214c15593cfc4cdb956a8d0
Prateek Sagar <prateeksagar640@gmail.com>
2026-09-17 09:30:52 +0530

    fix(admin): fix admin buttons, crash fixes, and ultra-resilient permanent team persistence

--- test_all_challenges.js
+++ test_all_challenges.js
@@ -11,12 +11,16 @@ const FLAGS = [
   { id: 'ch8', flag: 'logicCTF{wr0ng_w0rkfl0w_4lt3rn4t1v3_p4th_c0mpl3t3d}', pts: 50 },
   { id: 'ch9', flag: 'logicCTF{4rch1v3d_pr0j3ct_h1st0ry_m0d1f13d_unl0ck}', pts: 50 },
   { id: 'ch10', flag: 'logicCTF{wr0ng_n4m3_c0rr3ct_d0cum3nt_unm4sk3d}', pts: 50 },
-  { id: 'ch11', flag: 'logicCTF{ssrf_cl0ud_m3t4d4t4_cr3ds_pwn}', pts: 100 },
-  { id: 'ch12', flag: 'logicCTF{su1d_pr1v1l3g3_3sc4l4t10n_r00t}', pts: 100 },
+  { id: 'ch11', flag: 'logicCTF{ssrf_cl0ud_m3t4d4t4_cr3ds_pwn}', pts: 50 },
+  { id: 'ch12', flag: 'logicCTF{su1d_pr1v1l3g3_3sc4l4t10n_r00t}', pts: 50 },
   { id: 'ch13', flag: 'logicCTF{tim_berners_lee}', pts: 50 },
   { id: 'ch14', flag: 'logicCTF{m1ss1ng_t4sk_unl1st3d_c0mpl3t3d}', pts: 50 },
   { id: 'ch15', flag: 'logicCTF{unsck3dul4bl3_sh4r3_c4l3nd4r_p4th}', pts: 50 },
-  { id: 'ch16', flag: 'logicCTF{c4nc3ll3d_but_4ct1v3_st4t3_d1sc0v3r3d}', pts: 50 }
+  { id: 'ch16', flag: 'logicCTF{c4nc3ll3d_but_4ct1v3_st4t3_d1sc0v3r3d}', pts: 50 },
+  { id: 'ch17', flag: 'logicCTF{h1dd3n_n4m3_pl4c3h0ld3r_unm4sk3d}', pts: 50 },
+  { id: 'ch18', flag: 'logicCTF{3xp0rt_csv_tru3_fl4g_r3v34l3d}', pts: 50 },
+  { id: 'ch19', flag: 'logicCTF{f0rg0tt3n_m3nu_br4nd1ng_l0g0_ch4ng3d}', pts: 50 },
+  { id: 'ch20', flag: 'logicCTF{4rch1v3_v4ult_r3c0v3ry_c0mpl14nc3}', pts: 50 }
 ];
 
 function request(path, method = 'GET', body = null) {
